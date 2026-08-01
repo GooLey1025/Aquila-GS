605e2fd9990e52f1225f6e30aeda11e835db3a49
GooLey1025 <1261252636@qq.com>
2026-08-01 19:51:58 +0800

    fix Maize data bug: inconsistent sample name

--- benchmark/species_data/Maize1404/main.sh
+++ benchmark/species_data/Maize1404/main.sh
@@ -3,5 +3,7 @@ plink2   --vcf results/Maize1404.impute.biallelic.vcf.gz   --make-bed   --out Ma
 plink   --bfile Maize1404.impute   --indep-pairwise 1000 50 0.003   --out Maize1404.LD
 plink   --bfile Maize1404.impute   --extract Maize1404.LD.prune.in   --make-bed   --out Maize1404.LD
 plink   --bfile Maize1404.LD   --recode vcf bgz --out Maize1404.LD
-
+bcftools query -l Maize1404.LD.vcf.gz > sample_name.txt
+sed -i 's/^0_CUBIC_//' sample_name.txt 
+bcftools reheader -N sample_name.txt Maize1404.LD.vcf.gz -o Maize1404.LD.renmae.vcf.gz
 
